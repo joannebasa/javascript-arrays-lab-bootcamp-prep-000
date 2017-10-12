33637c721279079422e73ed887258677b12e5b7a
Joanne Basa <joannegbasa@gmail.com>
2017-10-12 20:49:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function kittens() {
-  return kittens;
-}
 
 function destructivelyAppendKitten(name) {
   kittens.push(name);
